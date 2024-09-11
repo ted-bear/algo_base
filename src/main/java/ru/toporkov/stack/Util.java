caa90ea60c79596f81e41afbc6bf703a88a0f907
teddy <trololo221@bk.ru>
2024-09-11 20:51:06 +0300

    Implement method for reverse calculations

--- src/main/java/ru/toporkov/stack/Util.java
+++ src/main/java/ru/toporkov/stack/Util.java
@@ -37,4 +37,42 @@ public class Util {
 
         return true;
     }
+
+    public static Integer calculateReverse(Stack<String> inStack) {
+        Stack<Integer> resultStack = new Stack<>();
+
+        while (inStack.size() >= 0) {
+            String currentElement = inStack.pop();
+            if (isNumber(currentElement)) {
+                resultStack.push(Integer.parseInt(currentElement));
+            } else if (currentElement.equals("=")) {
+                return resultStack.peek();
+            } else {
+                Integer firstElement = resultStack.pop();
+                Integer secondElement = resultStack.pop();
+                resultStack.push(calculate(firstElement, secondElement, currentElement));
+            }
+        }
+
+        return resultStack.peek();
+    }
+
+    private static Integer calculate(Integer first, Integer second, String operation) {
+        if (operation.equals("+")) {
+            return first + second;
+        } else if (operation.equals("*")) {
+            return first * second;
+        }
+
+        throw new RuntimeException("No such operation");
+    }
+
+    private static boolean isNumber(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
