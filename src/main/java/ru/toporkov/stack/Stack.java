4cd6682239387e1017de766a83028b0723b83667
teddy <trololo221@bk.ru>
2024-09-11 21:04:10 +0300

    Delete additional method from Stack.java

--- src/main/java/ru/toporkov/stack/Stack.java
+++ src/main/java/ru/toporkov/stack/Stack.java
@@ -3,17 +3,13 @@ package ru.toporkov.stack;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Stack<T extends Comparable<T>> {
+public class Stack<T> {
 
     private final List<T> list;
-    private final List<T> minList;
-    private Double sum;
     private int size;
 
     public Stack() {
         list = new LinkedList<>();
-        minList = new LinkedList<>();
-        sum = 0.;
     }
 
     public int size() {
@@ -24,7 +20,6 @@ public class Stack<T extends Comparable<T>> {
         if (size() != 0) {
             T last = list.getLast();
             list.removeLast();
-            minList.removeLast();
             size--;
             return last;
         }
@@ -32,37 +27,11 @@ public class Stack<T extends Comparable<T>> {
     }
 
     public void push(T val) {
-        if (val instanceof java.lang.Number) {
-            sum += Double.parseDouble(val.toString());
-        }
         size++;
         list.add(val);
-        updateMin(val);
-    }
-
-    public Double getMean() {
-        return size() == 0 ? null : sum / size();
     }
 
     public T peek() {
         return size() == 0 ? null : list.getLast();
-    }
-
-    public T getMin() {
-        if (!list.isEmpty()) {
-            return minList.getLast();
-        }
-
-        return null;
-    }
-
-    private void updateMin(T newElement) {
-        if (minList.isEmpty()) {
-            minList.add(newElement);
-        } else {
-            T currentMin = minList.getLast();
-            int compareRes = currentMin.compareTo(newElement);
-            minList.add(compareRes < 0 ? currentMin : newElement);
-        }
     }
 }
