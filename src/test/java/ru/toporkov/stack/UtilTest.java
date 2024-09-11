b7784e5665f3476d2da49e9da2f55ff95f9417ff
teddy <trololo221@bk.ru>
2024-09-11 20:55:01 +0300

    Add tests for Util.calculateReverse()

--- src/test/java/ru/toporkov/stack/UtilTest.java
+++ src/test/java/ru/toporkov/stack/UtilTest.java
@@ -2,8 +2,11 @@ package ru.toporkov.stack;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static ru.toporkov.stack.Util.calculateReverse;
 import static ru.toporkov.stack.Util.isBalanced;
 
 class UtilTest {
@@ -66,5 +69,48 @@ class UtilTest {
     void isBalanced_not_balanced_sequence_2() {
         String brackets = "((())}";
         assertFalse(isBalanced(brackets));
+    }
+
+    @Test
+    void calculateRevers_interesting_example() {
+        Stack<String> stack = new Stack<>();
+        stack.push("=");
+        stack.push("+");
+        stack.push("9");
+        stack.push("*");
+        stack.push("5");
+        stack.push("+");
+        stack.push("2");
+        stack.push("8");
+
+        assertEquals(59, calculateReverse(stack));
+    }
+
+    @Test
+    void calculateRevers_two_elements() {
+        Stack<String> stack = new Stack<>();
+        stack.push("=");
+        stack.push("+");
+        stack.push("9");
+        stack.push("5");
+
+        assertEquals(14, calculateReverse(stack));
+    }
+
+    @Test
+    void calculateRevers_single_element() {
+        Stack<String> stack = new Stack<>();
+        stack.push("=");
+        stack.push("9");
+
+        assertEquals(9, calculateReverse(stack));
+    }
+
+    @Test
+    void calculateRevers_empty_stack() {
+        Stack<String> stack = new Stack<>();
+        stack.push("=");
+
+        assertNull(calculateReverse(stack));
     }
 }
