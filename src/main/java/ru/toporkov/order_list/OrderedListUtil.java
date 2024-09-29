d6ca8f31d4196ab88f4f697c24fe17aba8a753c6
teddy <trololo221@bk.ru>
2024-09-29 16:07:16 +0300

    Add boolean argument to set ascending parameter in method to merge OrderedListUtil

--- src/main/java/ru/toporkov/order_list/OrderedListUtil.java
+++ src/main/java/ru/toporkov/order_list/OrderedListUtil.java
@@ -4,35 +4,47 @@ public class OrderedListUtil {
 
     static <T> OrderedList<T> mergeList(
             OrderedList<T> first,
-            OrderedList<T> second
+            OrderedList<T> second,
+            boolean isAsc
     ) {
-        OrderedList<T> merged = new OrderedList<>(true);
+        OrderedList<T> merged = new OrderedList<>(isAsc);
 
-        Node<T> firstPtr = first.is_ascending() ?
-                first.head :
-                first.tail;
-        Node<T> secondPtr = second.is_ascending() ?
-                second.head :
-                second.tail;
+        Node<T> firstPtr = first.is_ascending() ^ isAsc ?
+                first.tail :
+                first.head;
+
+        Node<T> secondPtr = second.is_ascending() ^ isAsc ?
+                second.tail :
+                second.head;
 
         while (firstPtr != null && secondPtr != null) {
-            if (first.compare(firstPtr.value, secondPtr.value) < 0) {
-                merged.add(firstPtr.value);
-                firstPtr = first.is_ascending() ? firstPtr.next : firstPtr.prev;
+            if (isAsc) {
+                if (first.compare(firstPtr.value, secondPtr.value) < 0) {
+                    merged.add(firstPtr.value);
+                    firstPtr = first.is_ascending() ? firstPtr.next : firstPtr.prev;
+                } else {
+                    merged.add(secondPtr.value);
+                    secondPtr = second.is_ascending() ? secondPtr.next : secondPtr.prev;
+                }
             } else {
-                merged.add(secondPtr.value);
-                secondPtr = second.is_ascending() ? secondPtr.next : secondPtr.prev;
+                if (first.compare(firstPtr.value, secondPtr.value) > 0) {
+                    merged.add(firstPtr.value);
+                    firstPtr = first.is_ascending() ? firstPtr.prev : firstPtr.next;
+                } else {
+                    merged.add(secondPtr.value);
+                    secondPtr = second.is_ascending() ? secondPtr.prev : secondPtr.next;
+                }
             }
         }
 
         while (firstPtr != null) {
             merged.add(firstPtr.value);
-            firstPtr = first.is_ascending() ? firstPtr.next : firstPtr.prev;
+            firstPtr = first.is_ascending() ^ isAsc ? firstPtr.prev : firstPtr.next;
         }
 
         while (secondPtr != null) {
             merged.add(secondPtr.value);
-            secondPtr = second.is_ascending() ? secondPtr.next : secondPtr.prev;
+            secondPtr = second.is_ascending() ^ isAsc ? secondPtr.prev : secondPtr.next;
         }
 
         return merged;
