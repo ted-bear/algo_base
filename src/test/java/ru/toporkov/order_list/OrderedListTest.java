8299f7a62831859a032ca08cb77fddc3684e37ba
Egor Toporkov <etoporkov@digimatics.ru>
2024-09-30 22:50:19 +0300

    Refactor tests: write method to create array

--- src/test/java/ru/toporkov/order_list/OrderedListTest.java
+++ src/test/java/ru/toporkov/order_list/OrderedListTest.java
@@ -13,107 +13,70 @@ class OrderedListTest {
 
     @Test
     void find_asc_head_el() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{12, 8, 7, 16});
 
         assertEquals(7, list.find(7).value);
     }
 
     @Test
     void find_asc_last_el() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{12, 8, 7, 16});
 
         assertEquals(16, list.find(16).value);
     }
 
     @Test
     void find_asc_middle_el() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{12, 8, 7, 16});
 
         assertEquals(8, list.find(8).value);
     }
 
     @Test
     void find_asc_not_existing_el() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{12, 8, 7, 16});
 
         assertNull(list.find(9));
     }
 
     @Test
     void find_desc_head_el() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{12, 8, 7, 16});
 
         assertEquals(7, list.find(7).value);
     }
 
     @Test
     void find_desc_last_el() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{12, 8, 7, 16});
 
         assertEquals(16, list.find(16).value);
     }
 
     @Test
     void find_desc_middle_el() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{12, 8, 7, 16});
 
         assertEquals(8, list.find(8).value);
     }
 
     @Test
     void find_desc_not_existing_el() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{12, 8, 7, 16});
 
         assertNull(list.find(9));
     }
 
     @Test
     void add_one_el() {
-        list = new OrderedList<>(false);
-        list.add(19);
+        list = createOrderedList(false, new int[]{19});
 
         assertEquals(19, list.tail.value);
     }
 
     @Test
     void add_in_asc_order() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{12, 8, 7, 16});
 
         assertEquals(7, list.head.value);
         assertEquals(8, list.head.next.value);
@@ -123,11 +86,7 @@ class OrderedListTest {
 
     @Test
     void add_in_desc_order() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{12, 8, 7, 16});
 
         assertEquals(16, list.head.value);
         assertEquals(12, list.head.next.value);
@@ -137,8 +96,7 @@ class OrderedListTest {
 
     @Test
     void delete_single_element() {
-        list = new OrderedList<>(false);
-        list.add(7);
+        list = createOrderedList(false, new int[]{7});
 
         list.delete(7);
 
@@ -148,11 +106,7 @@ class OrderedListTest {
 
     @Test
     void delete_first_min_in_desc_list() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{7, 8, 12, 16});
 
         list.delete(7);
 
@@ -162,11 +116,7 @@ class OrderedListTest {
 
     @Test
     void delete_first_min_in_asc_list() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{7, 8, 12, 16});
 
         list.delete(7);
 
@@ -176,11 +126,7 @@ class OrderedListTest {
 
     @Test
     void delete_first_max_in_asc_list() {
-        list = new OrderedList<>(true);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(true, new int[]{7, 8, 12, 16});
 
         list.delete(16);
 
@@ -190,11 +136,7 @@ class OrderedListTest {
 
     @Test
     void delete_first_max_in_desc_list() {
-        list = new OrderedList<>(false);
-        list.add(7);
-        list.add(12);
-        list.add(16);
-        list.add(8);
+        list = createOrderedList(false, new int[]{7, 8, 12, 16});
         assertEquals(4, list.count());
 
         list.delete(16);
@@ -250,8 +192,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_from_asc_single_el_list() {
-        list = new OrderedList<>(true);
-        list.add(2);
+        list = createOrderedList(true, new int[]{2});
         list.removeDuplicates();
 
         assertEquals(1, list.count());
@@ -260,9 +201,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_from_asc_pair_el_list() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
+        list = createOrderedList(true, new int[]{2, 2});
         list.removeDuplicates();
 
         assertEquals(1, list.count());
@@ -272,10 +211,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_from_asc_three_el_list() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-        list.add(2);
+        list = createOrderedList(true, new int[]{2, 2, 2});
         list.removeDuplicates();
 
         assertEquals(1, list.count());
@@ -285,10 +221,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_from_tail_asc() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-        list.add(1);
+        list = createOrderedList(true, new int[]{1, 2, 2});
         list.removeDuplicates();
 
         assertEquals(2, list.count());
@@ -298,10 +231,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_from_head_asc() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(1);
-        list.add(1);
+        list = createOrderedList(true, new int[]{2, 2, 1});
         list.removeDuplicates();
 
         assertEquals(2, list.count());
@@ -311,11 +241,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_without_dups() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(1);
+        list = createOrderedList(true, new int[]{2, 4, 3, 1});
         list.removeDuplicates();
 
         assertEquals(4, list.count());
@@ -325,15 +251,7 @@ class OrderedListTest {
 
     @Test
     void removeDuplicates_with_every_el() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-        list.add(3);
-        list.add(3);
-        list.add(4);
-        list.add(4);
-        list.add(1);
-        list.add(1);
+        list = createOrderedList(true, new int[]{2, 2, 3, 3, 4, 4, 1, 1});
 
         list.removeDuplicates();
 
@@ -346,206 +264,123 @@ class OrderedListTest {
 
     @Test
     void isSubList_emptyInputList() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-        list.add(3);
-        list.add(3);
+        list = createOrderedList(true, new int[]{2, 2, 3, 3});
 
         assertTrue(list.isSubList(new OrderedList<>(true)));
     }
 
     @Test
     void isSubList_singleSubListEl_positive() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-        list.add(3);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(2);
+        list = createOrderedList(true, new int[]{2, 2, 3});
+        var subList = createOrderedList(true, new int[]{2});
 
         assertTrue(list.isSubList(subList));
     }
 
     @Test
     void isSubList_singleSubListEl_negative() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-        list.add(3);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(1);
+        list = createOrderedList(true, new int[]{2, 2, 3});
+        var subList = createOrderedList(true, new int[]{1});
 
         assertFalse(list.isSubList(subList));
     }
 
     @Test
     void isSubList_singleListEl_singleSubListEl_positive() {
-        list = new OrderedList<>(true);
-        list.add(2);
+        list = createOrderedList(true, new int[]{2});
 
-        var subList = new OrderedList<Integer>(true);
-        subList.add(2);
+        var subList = createOrderedList(true, new int[]{2});
 
         assertTrue(list.isSubList(subList));
     }
 
     @Test
     void isSubList_singleListEl_singleSubListEl_negative() {
-        list = new OrderedList<>(true);
-        list.add(2);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(1);
+        list = createOrderedList(true, new int[]{2});
+        var subList = createOrderedList(true, new int[]{1});
 
         assertFalse(list.isSubList(subList));
     }
 
     @Test
     void isSubList_pairEl_pairSubListEl_positive() {
-        list = new OrderedList<>(true);
-        list.add(2);
-        list.add(2);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(2);
-        subList.add(2);
+        list = createOrderedList(true, new int[]{1, 2});
+        var subList = createOrderedList(true, new int[]{1, 2});
 
         assertTrue(list.isSubList(subList));
     }
 
     @Test
     void isSubList_pairEl_pairSubListEl_negative() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(2);
-        subList.add(2);
+        list = createOrderedList(true, new int[]{1, 2});
+        var subList = createOrderedList(true, new int[]{2, 2});
 
         assertFalse(list.isSubList(subList));
     }
 
     @Test
     void isSubList_manyEls_singleSubListEl_positive() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-        list.add(8);
-        list.add(9);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(3);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6, 8, 9});
+        var subList = createOrderedList(true, new int[]{3});
 
         assertTrue(list.isSubList(subList));
     }
 
     @Test
     void isSubList_manyEls_singleSubListEl_negative() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-        list.add(8);
-        list.add(9);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(4);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6, 8, 9});
+        var subList = createOrderedList(true, new int[]{4});
 
         assertFalse(list.isSubList(subList));
     }
 
     @Test
     void isSubList_manyEls_manySubListEl_positive() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-        list.add(8);
-        list.add(9);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(2);
-        subList.add(3);
-        subList.add(6);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6, 8, 9});
+        var subList = createOrderedList(true, new int[]{2, 3, 6});
 
         assertTrue(list.isSubList(subList));
     }
 
     @Test
     void isSubList_manyEls_manySubListEl_negative() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-        list.add(8);
-        list.add(9);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(4);
-        subList.add(3);
-        subList.add(6);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6, 8, 9});
+        var subList = createOrderedList(true, new int[]{4, 3, 6});
 
         assertFalse(list.isSubList(subList));
     }
 
     @Test
     void isSubList_manyEls_manySubListEls_negative() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-        list.add(8);
-        list.add(9);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(1);
-        subList.add(4);
-        subList.add(6);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6, 8, 9});
+        var subList = createOrderedList(true, new int[]{1, 4, 6});
 
         assertFalse(list.isSubList(subList));
     }
 
     @Test
     void isSubList_eqLength_positive() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(1);
-        subList.add(2);
-        subList.add(3);
-        subList.add(6);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6});
+        var subList = createOrderedList(true, new int[]{1, 2, 3, 6});
 
         assertTrue(list.isSubList(subList));
     }
 
     @Test
     void isSubList_eqLength_negative() {
-        list = new OrderedList<>(true);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(6);
-
-        var subList = new OrderedList<Integer>(true);
-        subList.add(2);
-        subList.add(2);
-        subList.add(3);
-        subList.add(6);
+        list = createOrderedList(true, new int[]{1, 2, 3, 6});
+        var subList = createOrderedList(true, new int[]{2, 2, 3, 6});
 
         assertFalse(list.isSubList(subList));
+    }
+
+    OrderedList<Integer> createOrderedList(boolean isAsc, int[] array) {
+        OrderedList<Integer> list = new OrderedList<>(isAsc);
+
+        for (int i : array) {
+            list.add(i);
+        }
+
+        return list;
     }
 }
