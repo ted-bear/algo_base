5c878bf2906c1ea232d51cd2861c5112ff31b945
Egor Toporkov <etoporkov@digimatics.ru>
2024-10-11 22:52:42 +0300

    Add tests for BloomFilter

--- src/test/java/ru/toporkov/blooms_filter/BloomFilterTest.java
+++ src/test/java/ru/toporkov/blooms_filter/BloomFilterTest.java
@@ -2,6 +2,7 @@ package ru.toporkov.blooms_filter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BloomFilterTest {
@@ -11,15 +12,42 @@ class BloomFilterTest {
     @Test
     void add_into_empty_filter() {
         filter = new BloomFilter(32);
-        String testinValue = "hash1";
+        String testingValue = "hash1";
 
-        filter.add(testinValue);
+        filter.add(testingValue);
 
-        assertTrue(filter.isValue(testinValue));
+        assertTrue(filter.isValue(testingValue));
     }
 
     @Test
-    void isValue() {
+    void add_into_empty_ten_rows() {
+        filter = new BloomFilter(32);
+        String[] testingVals = {
+                "0123456789",
+                "9012345678",
+                "8901234567",
+                "7890123456",
+                "6789012345",
+                "5678901234",
+                "4567890123",
+                "3456789012",
+                "2345678901",
+                "1234567890",
+        };
 
+        for (var value : testingVals) {
+            filter.add(value);
+        }
+
+        for (var value : testingVals) {
+            assertTrue(filter.isValue(value));
+        }
+    }
+
+    @Test
+    void isValue_empty() {
+        filter = new BloomFilter(32);
+
+        assertFalse(filter.isValue("4567890123"));
     }
 }
